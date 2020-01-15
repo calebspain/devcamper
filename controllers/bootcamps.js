85a0a3a36dd041a69a0a084c35a9f0904d3ebe67
Caleb Spain <crspain@uh.edu>
2020-01-14 21:01:08 -0600

    updated error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,7 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
 
         res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 };
 
@@ -28,7 +28,7 @@ exports.getBootcamp = async (req, res, next) => {
         res.status(200).json({ success: true, data: bootcamp });
     } catch (err) {
         // res.status(400).json({ success: false });
-        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        next(err);
     }
 };
 
@@ -44,7 +44,7 @@ exports.createBootcamp = async (req, res, next) => {
             data: bootcamp
         });
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 };
 
@@ -64,7 +64,7 @@ exports.updateBootcamp = async (req, res, next) => {
 
         res.status(200).json({ success: true, data: bootcamp });
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 };
 
@@ -81,6 +81,6 @@ exports.deleteBootcamp = async (req, res, next) => {
 
         res.status(200).json({ success: true, data: {} });
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 };
